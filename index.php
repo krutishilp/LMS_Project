fb2c80d1aaec9f7ceb1ba85d8c29e1753a3f0a2a
Prathamesh Dhobale <dhobale.1602prathamesh@gmail.com>
2021-03-16 22:48:18 +0530

    Normalised Database - Admin

--- index.php
+++ index.php
@@ -1,160 +1,160 @@
-<?php session_start()?>
+<?php session_start() ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Home</title>
- <?php include 'links.php'; ?>	
- <?php include 'style.php';?>
+  <title>Home</title>
+  <?php include 'links.php'; ?>
+  <?php include 'style.php'; ?>
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand mx-auto" href="#">E-Learning</a>
-    </nav>
-    <div class="container-fluid pad2">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand mx-auto" href="#">E-Learning</a>
+  </nav>
+  <div class="container-fluid pad2">
     <h2>Krutishil Purkar</h2>
-      <div class="row pad">
-        <div class="col-lg-6 offset-lg-3 sm-auto md-auto">
-          <div class="row">
-             <div class="col abtpad">
-               <form action="index.php" method="POST" class="fc">
-                 <h3>Sign-up</h3><hr>
-                  <input type="text" name="name"  placeholder="Full-Name" required><br><br>
-                  <input type="email" name="email" placeholder="Email" required><br><br>
-                  <input type="password" name="password" placeholder="Password" required><br><br>
-                  <select name="dept" id="dept">
-                    <option value="">Select Department</option>
-                    <option value="mech">Mechanical</option>
-                    <option value="comp">Computer</option>
-                    <option value="it">IT</option>
-                  </select>
-                  <input type="text" name="year"  placeholder="Full-Name" required><br><br>
-                  <input type="text" name="prn"  placeholder="Full-Name" required><br><br>
-                  <input type="submit" name="su" value="Sign-Up">
-               </form>
-             </div>
-             <div class="col abtpad">
-               <form action="index.php" method="POST" class="fc">
-                 <h3>Login</h3><hr>
-                 <input type="email" name="uemail" placeholder="Email"><br><br>
-                 <input type="password" name="upassword" placeholder="Password"><br><br><br>
-                 <input type="submit" name="lg" value="Log-in"><br><br>
-                
-               </form>
-             </div>
-           </div>
-           <div class="abtpad">
-           <div class=" fc">
-          <h3>About</h3>
-          <hr>
-          <P>To create quality information technology professionals through superior academic environment.<br>
-            To incorporate the IT fundamentals in students to be successful in their career.<br>
-            To motivate the students for higher studies ,research and entrepreneurship.<br>
-          To provide IT services to society .<br><br>
-        </P>
-          <h3>FAQ</h3>
-         <hr>
-          <p>
-          1)<b>What is E-learning?</b><br>
-            A platform for students of E-Learning to interact with teachers with help of usefull resources like PDF notes,Videos and PPTs.<br><br>
-          2)<b>What will you get?</b><br>
-                 Helping students re-visit the lecture to revise and understand concepts aswell as provide necessary resources for exam prep. 
-                 <br>Unit Wise:<br>
-                 <ul>
-                   <li>Video Lectures</li>
-                   <li>PDF Notes</li>
-                   <li>PPT's</li>
-                   <li>Quizzes</li>
-                 </ul>
-          </p>
+    <div class="row pad">
+      <div class="col-lg-6 offset-lg-3 sm-auto md-auto">
+        <div class="row">
+          <div class="col abtpad">
+            <form action="index.php" method="POST" class="fc">
+              <h3>Sign-up</h3>
+              <hr>
+              <input type="text" name="name" placeholder="Full-Name" required><br><br>
+              <input type="email" name="email" placeholder="Email" required><br><br>
+              <input type="password" name="password" placeholder="Password" required><br><br>
+              <select name="dept" id="dept">
+                <option value="">Select Department</option>
+                <option value="mech">Mechanical</option>
+                <option value="comp">Computer</option>
+                <option value="it">IT</option>
+              </select>
+              <input type="text" name="year" placeholder="Full-Name" required><br><br>
+              <input type="text" name="prn" placeholder="Full-Name" required><br><br>
+              <input type="submit" name="su" value="Sign-Up">
+            </form>
+          </div>
+          <div class="col abtpad">
+            <form action="index.php" method="POST" class="fc">
+              <h3>Login</h3>
+              <hr>
+              <input type="email" name="uemail" placeholder="Email"><br><br>
+              <input type="password" name="upassword" placeholder="Password"><br><br><br>
+              <select name="role" id="role">
+              <option value="">Select</option>
+                <option value="admin">Admin</option>
+                <option value="teachers">Teacher</option>
+                <option value="student">Student</option>
+              </select>
+              <input type="submit" name="lg" value="Log-in"><br><br>
+
+            </form>
+          </div>
+        </div>
+        <div class="abtpad">
+          <div class=" fc">
+            <h3>About</h3>
+            <hr>
+            <P>To create quality information technology professionals through superior academic environment.<br>
+              To incorporate the IT fundamentals in students to be successful in their career.<br>
+              To motivate the students for higher studies ,research and entrepreneurship.<br>
+              To provide IT services to society .<br><br>
+            </P>
+            <h3>FAQ</h3>
+            <hr>
+            <p>
+              1)<b>What is E-learning?</b><br>
+              A platform for students of E-Learning to interact with teachers with help of usefull resources like PDF notes,Videos and PPTs.<br><br>
+              2)<b>What will you get?</b><br>
+              Helping students re-visit the lecture to revise and understand concepts aswell as provide necessary resources for exam prep.
+              <br>Unit Wise:<br>
+              <ul>
+                <li>Video Lectures</li>
+                <li>PDF Notes</li>
+                <li>PPT's</li>
+                <li>Quizzes</li>
+              </ul>
+            </p>
+          </div>
         </div>
       </div>
-         </div>
-    </div> 
-      </div>
-      <div class="navbar navbar-expand-lg navbar-dark bg-dark" id="footer">
-        <a class="navbar-brand mx-auto">Copyright 2020, Nishad Raisinghani.</a>
-      </div>
+    </div>
+  </div>
+  <div class="navbar navbar-expand-lg navbar-dark bg-dark" id="footer">
+    <a class="navbar-brand mx-auto">...</a>
+  </div>
 </body>
+
 </html>
 
 <?php
 include 'connection.php';
 
 //-------------------------------------------- Sign-Up ------------------------------------------	
-if(isset($_POST['su']))
-{
+if (isset($_POST['su'])) {
 
-	$uname=$_POST['name'];
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	$dept=$_POST['dept'];
-	$year=$_POST['year'];
-	$prn=$_POST['prn'];
-//	$mname=$_POST['mname'];
-  
-	$signup="INSERT INTO user (name, email, pass) VALUES ('$uname','$email','$password')";
-	if($run=mysqli_query($conn,$signup))
-	{
+  $uname = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $dept = $_POST['dept'];
+  $year = $_POST['year'];
+  $prn = $_POST['prn'];
+  //	$mname=$_POST['mname'];
 
-      echo '<script type="text/javascript">alert("Done. Please Login.")</script>';
-    
-	}
-	else
-	{
-		echo '<script type="text/javascript">alert("Not Done. Please Try Again.")</script>';
-    
-	}
+  $signup = "INSERT INTO user (name, email, pass) VALUES ('$uname','$email','$password')";
+  if ($run = mysqli_query($conn, $signup)) {
+
+    echo '<script type="text/javascript">alert("Done. Please Login.")</script>';
+  } else {
+    echo '<script type="text/javascript">alert("Not Done. Please Try Again.")</script>';
+  }
 }
 
 //------------------------------------------------------------------------------------------------------
-if(isset($_POST['lg']))
-{
+if (isset($_POST['lg'])) {
 
-	$email=$_POST['uemail'];
-	$password=$_POST['upassword'];
-	
-	$login="SELECT * FROM user WHERE (email='$email' AND pass='$password')";
-	if($run2=mysqli_query($conn,$login))
-	{
-      $num=mysqli_num_rows($run2);
-      if($num==1)
-      {
-      	$row=mysqli_fetch_assoc($run2);
-  		if($row['pos']=='T')
-  		{
-  		 $_SESSION['teacher_user_email']=$email;
-  	     $_SESSION['teacher_user_pass']=$password;
-  	     $_SESSION['teacher_user_name']=$row['name'];
-  		   echo "<script type='text/javascript'>location.replace('Teacher/teacher.php')</script>";
-  	    }
-        elseif($row['pos']=='A')
-       {
-       $_SESSION['admin_user_email']=$email;
-         $_SESSION['admin_user_pass']=$password;
-         $_SESSION['admin_user_name']=$row['name'];
-       echo "<script type='text/javascript'>location.replace('Admin/admin.php')</script>";
-        }
-      
-  	    else
-  	    {
-  		$_SESSION['student_user_email']=$email;
-  	    $_SESSION['student_user_pass']=$password;
-  	    $_SESSION['student_user_name']=$row['name'];
-        $_SESSION['student_user_year']=$row['year'];
-      $_SESSION['student_user_dept']=$row['dept'];
-  		echo "<script type='text/javascript'>location.replace('Student/student.php')</script>";
-        }   	
-   	 }
-     else
-     {
-      	echo '<script type="text/javascript">alert("Email Or Password is wrong check again or contact admin.");</script>;';
-     }    
-	}
-	else
-	{
-		echo '<script type="text/javascript">alert("Email Or Password is wrong check again or contact admin.");</script>';
-    
-	}
+  $email = $_POST['uemail'];
+  $password = $_POST['upassword'];
+  $role = $_POST['role'];
+
+  $login = "";
+  if ($role == "admin") {
+    $login = "SELECT * FROM admin WHERE (email='$email' AND password='$password')";
+  } elseif ($role == "teacher") {
+    $login = "SELECT * FROM teacher WHERE (email='$email' AND password='$password')";
+  } else {
+    $login = "SELECT * FROM student WHERE (email='$email' AND password='$password')";
+  }
+
+  if ($run2 = mysqli_query($conn, $login)) {
+    $num = mysqli_num_rows($run2);
+    if ($num == 1) {
+      $row = mysqli_fetch_assoc($run2);
+      if ($role== 'teacher') {
+        $_SESSION['teacher_user_email'] = $email;
+        $_SESSION['teacher_user_pass'] = $password;
+        $_SESSION['teacher_user_name'] = $row['name'];
+        echo "<script type='text/javascript'>location.replace('Teacher/teacher.php')</script>";
+      } elseif ($role == 'admin') {
+        $_SESSION['admin_user_email'] = $email;
+        $_SESSION['admin_user_pass'] = $password;
+        $_SESSION['admin_user_name'] = $row['name'];
+        echo "<script type='text/javascript'>location.replace('Admin/admin.php')</script>";
+      } else {
+        $_SESSION['student_user_email'] = $email;
+        $_SESSION['student_user_pass'] = $password;
+        $_SESSION['student_user_name'] = $row['name'];
+        $_SESSION['student_user_year'] = $row['year'];
+        $_SESSION['student_user_dept'] = $row['dept'];
+        echo "<script type='text/javascript'>location.replace('Student/student.php')</script>";
+      }
+    } else {
+      echo '<script type="text/javascript">alert("Email Or Password is wrong check again or contact admin.'.$login.'");</script>;';
+    }
+  } else {
+    echo '<script type="text/javascript">alert("Email Or Password is wrong check again or contact admin.'.$login.'");</script>';
+  }
 }
 
 ?>
